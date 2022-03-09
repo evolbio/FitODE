352544039f899de3c7d0887bdf8662587ec48710
Steve Frank <saf@stevefrank.org>
2022-03-08 16:24:05 -0800

    Added spline fitting, then try to optimize against splines rather than original data.

--- lynx_hare_ode.jl
+++ lynx_hare_ode.jl
@@ -1,24 +1,35 @@
 using DiffEqFlux, DifferentialEquations, Plots, GalacticOptim, CSV, DataFrames, Statistics
-# using Interpolations
+
+use_splines = true
 
 # number of variables to track in ODE, first two are hare and lynx
-n = 5 # must be >= 2
+n = 3 # must be >= 2
 nsqr = n*n
 # n^2 matrix for pairwise interactions plus vector n for individual growth
 p = 0.1*rand(nsqr + n);
 
-df = CSV.read("/Users/steve/Desktop/current/hare_lynx_data.csv", DataFrame);
+df = CSV.read("/Users/steve/sim/zzOtherLang/julia/autodiff/lynx_hare/lynx_hare_data.csv",
+					DataFrame);
 ode_data = permutedims(Array{Float32}(df[:,2:3]));
 # take log and then normalize by average
 ode_data = log.(ode_data)
 ode_data = ode_data .- mean(ode_data)
 
-u0 = ode_data[:,1] # Initial condition, first time point in data
-u0 = vcat(u0,rand(Float32,n-2)*30)
-
 datasize = length(ode_data[1,:]) # Number of time points
 tspan = (0.0f0, Float32(datasize-1)) # Time range
 tsteps = range(tspan[1], tspan[2], length = datasize) # Split time range into equal steps
+
+# spline
+if use_splines
+	using Interpolations
+	hspline = CubicSplineInterpolation(tsteps,ode_data[1,:])
+	lspline = CubicSplineInterpolation(tsteps,ode_data[2,:])
+	#plot((0:0.1:90),hspline.(0:0.1:90)); plot!((0:0.1:90),lspline.(0:0.1:90))
+	ode_data = vcat(hspline.(tsteps)',lspline.(tsteps)');
+end
+
+u0 = ode_data[:,1] # Initial condition, first time point in data
+u0 = vcat(u0,randn(Float32,n-2))
 
 swish(x) = x ./ (exp.(-x) .+ 1.0)
 
@@ -44,7 +55,8 @@ end
 # try training in parts, following https://diffeqflux.sciml.ai/dev/examples/local_minima/
 function loss(p, i, prob)
   #pred = solve(prob, Tsit5(), p=p, saveat = tsteps[tsteps .<= i], maxiters=100000)
-  pred = solve(prob, p=p, saveat = tsteps[tsteps .<= i], maxiters=100000)
+  # see https://diffeqflux.sciml.ai/dev/ControllingAdjoints/ for sensealg
+  pred = solve(prob, p=p, saveat = tsteps[tsteps .<= i], maxiters=300000)
   # use first two variables to match lynx & hare data
   # other variables are there to help the fitting
   loss = sum(abs2, pred[1:2,:] .- ode_data[:,1:length(pred[1,:])])
@@ -52,7 +64,7 @@ function loss(p, i, prob)
 end
 
 #for i in 9.0:9.0:18.0
-for i in 6.0:6.0:90.0
+for i in 9.0:9.0:90.0
 	println(i)
 	#display(p)
 	prob = ODEProblem(ode!, u0, (0.0,i), p)
@@ -60,19 +72,3 @@ for i in 6.0:6.0:90.0
 		cb = callback, maxiters = 2000)
 	p = result.u
 end
-
-##############################################################
-# train in one round over all time
-
-# prob = ODEProblem(ode!, u0, tspan, p)
-
-# function loss(p)
-#   pred = solve(prob, Tsit5(), p=p, saveat = tsteps, maxiters=4000)
-#   # use first two variables to match lynx & hare data
-#   # other variables are there to help the fitting
-#   loss = sum(abs2, pred[1:2,:] .- ode_data)
-#   return loss, pred
-# end
-
-# result = DiffEqFlux.sciml_train(p -> loss(w(p,n)), p; cb = callback)
-
