587ce94074593e492d3cc0b8a9c20c8c962dbbd8
Steve Frank <saf@stevefrank.org>
2022-03-10 07:49:49 -0800

    Working version of NODE, including updated plots, splines, increasing number of intermediate points to fit. Tries to fit all data in one go. Next, will fit in pieces.

--- lynx_hare_node.jl
+++ lynx_hare_node.jl
@@ -1,17 +1,34 @@
-using DiffEqFlux, DifferentialEquations, Plots, GalacticOptim, CSV, DataFrames
+using DiffEqFlux, DifferentialEquations, Plots, GalacticOptim, CSV, DataFrames,
+		Statistics
 
 # number of variables to track in ODE, first two are hare and lynx
 n = 10 # must be >= 2
-
-df = CSV.read("/Users/steve/Desktop/current/hare_lynx_data.csv", DataFrame);
+use_splines = true
+# points per year
+pts = if use_splines 2 else 1 end
+ 
 ode_data = permutedims(Array{Float32}(df[:,2:3]));
 
-u0 = ode_data[:,1] # Initial condition, first time point in data
-u0 = vcat(u0,rand(Float32,n-2)*30)
+# take log and then normalize by average
+ode_data = log.(ode_data)
+ode_data = ode_data .- mean(ode_data)
 
 datasize = length(ode_data[1,:]) # Number of time points
 tspan = (0.0f0, Float32(datasize-1)) # Time range
-tsteps = range(tspan[1], tspan[2], length = datasize) # Split time range into equal steps
+tsteps = range(tspan[1], tspan[2], length = datasize) # Split to equal steps
+
+# fit spline to data and use fitted data to train
+if use_splines
+	using Interpolations
+	hspline = CubicSplineInterpolation(tsteps,ode_data[1,:])
+	lspline = CubicSplineInterpolation(tsteps,ode_data[2,:])
+	#plot((0:0.1:90),hspline.(0:0.1:90)); plot!((0:0.1:90),lspline.(0:0.1:90))
+	tsteps = range(tspan[1], tspan[2], length = 1+pts*(datasize-1)) # Split to equal steps
+	ode_data = vcat(hspline.(tsteps)',lspline.(tsteps)');
+end
+
+u0 = ode_data[:,1] # Initial condition, first time point in data
+u0 = vcat(u0,rand(Float32,n-2)*30)
 
 # Make a neural net with a NeuralODE layer
 dudt2 = FastChain(FastDense(n, 50, tanh), FastDense(50, n))
@@ -31,8 +48,11 @@ end
 callback = function (p, l, pred; doplot = true)
   display(l)
   # plot current prediction against data
-  plt = scatter(tsteps, ode_data[1,:], label = "data")
-  scatter!(plt, tsteps, pred[1,:], label = "prediction")
+  plt = plot(size=(600,800), layout=(2,1))
+  scatter!(tsteps, ode_data[1,:], label = "hare", subplot=1)
+  scatter!(plt, tsteps, pred[1,:], label = "pred", subplot=1)
+  scatter!(tsteps, ode_data[2,:], label = "lynx", subplot=2)
+  scatter!(plt, tsteps, pred[2,:], label = "pred", subplot=2)
   if doplot
     display(plot(plt))
   end
