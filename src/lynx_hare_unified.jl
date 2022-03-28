8f70ef81fb505212df6412ab85b7382c45ee2d9b
Steve Frank <saf@stevefrank.org>
2022-03-28 14:40:06 -0700

    Preliminary testing shows that NODE seems to work in unified code with and without optimizing dummy u0 values.

--- src/lynx_hare_unified.jl
+++ src/lynx_hare_unified.jl
@@ -26,11 +26,13 @@ layer_size = 20,	# size of layers for NODE
 
 # number of variables to track in (N)ODE, first two are hare and lynx
 n = 3, 					# must be >= 2, number dummy variables is n-2
-opt_dummy_u0 = false,	# optimize dummy init values instead of using rand values
+opt_dummy_u0 = true,	# optimize dummy init values instead of using rand values
 
-# larger tolerances are faster but errors make gradient descent more challenging
-rtol = 1e-10,		# relative tolerance for ODE solver
-atol = 1e-12,		# absolute tolerance for ODE solver
+# Larger tolerances are faster but errors make gradient descent more challenging
+# However, fit is sensitive to tolerances, seems NODE may benefit from fluctuations
+# with larger tolerances whereas ODE needs smoother gradient from smaller tolerances??
+rtol = 1e-2,		# relative tolerance for solver, ODE -> ~1e-10, NODE -> ~1e-2 or -3
+atol = 1e-3,		# absolute tolerance for solver, ODE -> ~1e-12, NODE -> ~1e-3 or -4
 adm_learn = 0.0005,	# Adam rate, >=0.0002 for Tsit5, >=0.0005 for TRBDF2, change as needed
 max_it = 500,		# max iterates for each incremental learning step
 print_grad = true,	# show gradient on terminal, requires significant overhead
@@ -41,8 +43,10 @@ out_file = "/Users/steve/Desktop/" * now_name * ".jld2",
 
 git_vers = chomp(read(`git -C $proj_dir rev-parse --short HEAD`,String)),
 
-generate_rand_seed = false,
-rand_seed = 0x695db8870561193d,
+# if true, program generates new random seed, otherwise uses rand_seed
+# these defaults can be overridden by args to set_rand_seed()
+generate_rand_seed = true,
+rand_seed = 0x0861a3ea66cd3e9a,
 
 wt_base = 1.1,		# good default is 1.1
 wt_trunc = 1e-2,	# truncation for weights
@@ -50,7 +54,7 @@ wt_trunc = 1e-2,	# truncation for weights
 # would be worthwhile to experiment with various solvers
 # see https://diffeq.sciml.ai/stable/solvers/ode_solve/
 # TRBDF2() for large tol, Rodas4P() for small tol; Tsit5() faster? but check instability
-solver = Tsit5(), 
+solver = TRBDF2(), 
 
 # Activation function:
 # tanh seems to give good fit, perhaps best fit, and maybe overfit
@@ -121,40 +125,31 @@ activate = if (S.activate == 1) identity elseif (S.activate == 2) tanh
 # of p
 if S.use_node
 	dudt = FastChain(FastDense(S.n, S.layer_size, activate), FastDense(S.layer_size, S.n))
-	problem(p, u_init, tspn, ts) = 
-		NeuralODE(dudt, tspn, S.solver, saveat = ts, reltol = S.rtol, abstol = S.atol)
 	if S.opt_dummy_u0
 		function predict(p, prob, u_init)
 		  u_new = vcat(u_init,p[1:S.n-2])
 		  Array(prob(u_new, p[S.n-1:end]))
 		end
 	else
-		function predict(p, prob, u_init)
-		  Array(prob(u_init, p))
-		end
+		predict(p, prob, u_init) = Array(prob(u_init, p))
 	end
 else # ode instead of node, resove u_init and p in prob arg
-	function predict(p, prob, u_init)
-	  solve(prob, S.solver, p=p)
-	end
 	function ode!(du, u, p, t)
 		s = reshape(p[1:nsqr], S.n, S.n)
 		du .= activate.(s*u .- p[nsqr+1:end])
 	end
 	if S.opt_dummy_u0
-		problem(p, u_init, tspn, ts) = ODEProblem(ode!, vcat(u_init,p[1:S.n-2]),
-			tspn, p[S.n-1:end], saveat = ts, reltol = S.rtol, abstol = S.atol)
+		predict(p, prob, u_init) =
+			solve(prob, S.solver, u0=vcat(u_init,p[1:S.n-2]), p=p[S.n-1:end])
 	else
-		problem(p, u_init, tspn, ts) = 
-			ODEProblem(ode!, u_init, tspn, p, saveat = ts, reltol = S.rtol, abstol = S.atol)
+		predict(p, prob, u_init) = solve(prob, S.solver, p=p)
 	end
 end
 
-callback = function (p, l, pred, prob, u_init, w, tspn, tstp; doplot = true, show_lines = false,
-						show_third = false)
-	println("Callback")
+function callback(p, l, pred, prob, u_init, w;
+						doplot = true, show_lines = false, show_third = false)
   if (S.print_grad)
-  	grad = gradient(p->loss(p,u_init,w,tspn,tstp)[1], p)[1]
+  	grad = gradient(p->loss(p,u_init,w,prob)[1], p)[1]
   	gnorm = sqrt(sum(abs2, grad))
   	println(@sprintf("%5.3e; %5.3e", l, gnorm))
   else
@@ -180,15 +175,13 @@ callback = function (p, l, pred, prob, u_init, w, tspn, tstp; doplot = true, sho
   return false
 end
 
-function loss(p, u_init, w, tspn, tstp)
-	prob = problem(p, u_init, tspn, tstp)
+function loss(p, u_init, w, prob)
 	pred_all = predict(p, prob, u_init)
 	pred = pred_all[1:2,:]	# First rows are hare & lynx, others dummies
 	pred_length = length(pred[1,:])
 	if pred_length != length(w[1,:]) println("Mismatch") end
-	println("OK loss")
 	loss = sum(abs2, w[:,1:pred_length] .* (ode_data[:,1:pred_length] .- pred))
-	return loss, pred_all, prob, u_init, w, tspn, tstp
+	return loss, pred_all, prob, u_init, w
 end
 
 function weights(a; b=10, trunc=S.wt_trunc) 
@@ -204,58 +197,57 @@ end
 
 beta_a = 1:1:wt_steps
 set_rand_seed();
-if !S.use_node p_init = 0.1*rand(nsqr + S.n) end;	# n^2 matrix plus vector n for individual growth
+if !S.use_node p_init = 0.1*rand(nsqr + S.n) end; # n^2 matrix plus n for individual growth
 for i in 1:length(beta_a)
 	global result
 	println(beta_a[i])
 	w = weights(S.wt_base^beta_a[i]; trunc=S.wt_trunc)
 	last_time = tsteps[length(w[1,:])]
 	ts = tsteps[tsteps .<= last_time]
-	# increase p length by adding u0 for dummy dimensions
+	# for ODE, will redefine u0 and p in call to solve, so just need right sizes here
+	prob = S.use_node ?
+				NeuralODE(dudt, (0.0,last_time), S.solver, saveat = ts, 
+					reltol = S.rtol, abstol = S.atol) :
+				ODEProblem(ode!, u0, (0.0,last_time), p=p_init,
+					saveat = ts, reltol = S.rtol, abstol = S.atol)
 	if (i == 1)
-		if S.use_node
-			prob = NeuralODE(dudt, (0.0,last_time), S.solver, saveat = ts,
-						reltol = S.rtol, abstol = S.atol)
-			p = prob.p
-		else
-			p = S.opt_dummy_u0 ? vcat(randn(S.n-2),p_init) : p_init
-		end
+		p = S.use_node ? prob.p : p_init
+		if S.opt_dummy_u0 p = vcat(randn(S.n-2),p) end
 	else
 		p = result.u
 	end
-
-	println("OK1")
-	println(loss(p,u0,w,(0,last_time),ts)[1])
-  	grad = gradient(p->loss(p,u0,w,(0,last_time),ts)[1], p)[1]
-	println("OK2")
-  	println(sqrt(sum(abs2, grad)))
-
-	result = DiffEqFlux.sciml_train(p -> loss(p,u0,w,(0,last_time),ts), p,
+	result = DiffEqFlux.sciml_train(p -> loss(p,u0,w,prob), p,
 					ADAM(S.adm_learn); cb = callback, maxiters=S.max_it)
 end
 
 # do additional optimization round with equal weights at all points
+prob = S.use_node ?
+			NeuralODE(dudt, tspan, saveat = tsteps, 
+				reltol = S.rtol, abstol = S.atol) :
+			ODEProblem(ode!, u0, tspan, p=p_init,
+				saveat = tsteps, reltol = S.rtol, abstol = S.atol)
+
 ww = ones(2,length(tsteps))
 p1 = result.u
-lossval = loss(p1,u0,ww,tspan,tsteps);
+lossval = loss(p1,u0,ww,prob);
 loss1 = lossval[1]
 pred1 = lossval[2]
 
-result2 = DiffEqFlux.sciml_train(p -> loss(p,u0,ww,tspan,tsteps), result.u, ADAM(S.adm_learn);
-			cb = callback, maxiters=S.max_it)
+result2 = DiffEqFlux.sciml_train(p -> loss(p,u0,ww,prob), result.u,
+			ADAM(S.adm_learn); cb = callback, maxiters=S.max_it)
 
 p2 = result2.u
-lossval = loss(p2,u0,ww,tspan,tsteps);
+lossval = loss(p2,u0,ww,prob);
 loss2 = lossval[1]
 pred2 = lossval[2]
 
-grad = gradient(p->loss(p,u0,ww,tspan,tsteps)[1], p2)
+grad = gradient(p->loss(p,u0,ww,prob)[1], p2)
 
-result3 = DiffEqFlux.sciml_train(p -> loss(p,u0,ww,tspan,tsteps),p2,BFGS(),
+result3 = DiffEqFlux.sciml_train(p -> loss(p,u0,ww,prob),p2,BFGS(),
 			cb = callback, maxiters=S.max_it)
 
 p3 = result3.u
-lossval = loss(p3,u0,ww,tspan,tsteps);
+lossval = loss(p3,u0,ww,prob);
 loss3 = lossval[1]
 pred3 = lossval[2]
 
